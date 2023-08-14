43e6a86b519172bc8290c08293ce891e7953480f
azimochilov <azimochilov226@gmail.com>
2023-08-14 13:33:21 +0500

    some changes

--- WebApp/Data/Repositories/Repository.cs
+++ WebApp/Data/Repositories/Repository.cs
@@ -1,4 +1,37 @@
-﻿namespace WebApp.Data.Repositories;
-public class Repository
+﻿using System.Linq.Expressions;
+using WebApp.Data.IRepositories;
+using WebApp.Domain.Commons;
+
+namespace WebApp.Data.Repositories;
+public class Repository<T> : IRepository<T> where T : Auditable
 {
+    public ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<T> InsertAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
 }
