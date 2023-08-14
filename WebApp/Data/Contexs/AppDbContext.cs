0bf4aaa4cd538c167e7a95a273519d14060e8f27
Azim Ochilov <azimochilov29@gmail.com>
2023-08-14 13:22:09 +0500

    yteasd

---
+++ WebApp/Data/Contexs/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Domain.Entities;
+
+namespace WebApp.Data.Contexs;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+}
---
+++ WebApp/Data/IRepositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using WebApp.Domain.Commons;
+
+namespace WebApp.Data.IRepositories;
+public interface IRepository<T> where T : Auditable
+{
+    ValueTask<T> InsertAsync(T entity);
+    T Update(T entity);
+    IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, string[] includes = null);
+    ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null);
+    ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression);
+
+    ValueTask SaveAsync();
+}
