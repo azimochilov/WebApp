e31d4cbb508499bedbb3fce146b5774dbdaefcba
azimochilov <azimochilov226@gmail.com>
2023-08-17 13:45:10 +0500

    app setings and program.cs are ready

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using WebApp.Data.Contexs;
 using WebApp.Data.IRepositories;
 using WebApp.Data.Repositories;
+using WebApp.Service.Interfaces;
 using WebApp.Service.Mappers;
+using WebApp.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +14,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IAuthorService,AuthorService>();
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+builder.Services.AddScoped<IGenreService, GenreService>();
+builder.Services.AddScoped<IPublisherService, PublisherService>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 var app = builder.Build();
